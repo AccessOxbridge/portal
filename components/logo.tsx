c278eda780f11f8344e6a7614150637737cb6bcd
Govind <yakshitchhipa@gmail.com>
2026-01-23 06:14:32 +0530

    feat: Add custom scrollbar styles and enhance Logo component with customizable text color

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/utils/lib";
 
-export function Logo({ className }: { className?: string }) {
-    return (
-        <Link
-          href="/"
-          aria-label="Access Oxbridge"
-          className={cn("col-span-1 flex items-center justify-center transition-opacity gap-2", className)}
-        >
-          <Image src="/logo.webp" alt="Access Oxbridge" width={32} height={32} />
-          <h1 className={`text-lg hidden md:flex sm:text-2xl tracking-tighter text-center whitespace-nowrap`}>
-            Access Oxbridge
-          </h1>
-        </Link>
-    )
+export function Logo({ className, textColor = "text-gray-900" }: { className?: string, textColor?: string }) {
+  return (
+    <Link
+      href="/"
+      aria-label="Access Oxbridge"
+      className={cn("col-span-1 flex items-center justify-center transition-opacity gap-2", className)}
+    >
+      <Image src="/logo.webp" alt="Access Oxbridge" width={32} height={32} />
+      <h1 className={cn("text-lg hidden md:flex sm:text-2xl tracking-tighter text-center whitespace-nowrap", textColor)}>
+        Access Oxbridge
+      </h1>
+    </Link>
+  )
 }
